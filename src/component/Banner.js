803bf90b4789d828490633b5601cc6d545ab474b
ayomide <folorunshoayomide46@gmail.com>
2022-10-12 00:15:12 +0100

    updated banner sector

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
 import { Container, Row , Col} from 'react-bootstrap';
 import { ArrowRightCircle } from 'react-bootstrap-icons'
-import headerImg from '../asset/img/header-img.svg'
+import headerImg from '../asset/img/header-img1.svg'
+import { Link } from 'react-router-dom'
 
 
 
 const Banner = () =>{
-
+    const mailto = 'mailto:folorunshoayomide46@gmail.com'
     return (
       <section className='banner' id='home'>
         <Container>
@@ -15,20 +15,28 @@ const Banner = () =>{
             <Col xs={12} md={6} xl={7}>
               <span className='tagline'> Welcome to my Portfolio</span>
               <h1>
-                Hi, I'm Ayomide Folorunsho
-                <span className='wrap'> a web developer</span>
+                Hi, I'm Ayomide Folorunsho.
+                <h2>Full-stack web developer</h2>
               </h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
                 officia debitis consequuntur porro obcaecati dolor voluptatibus
                 vitae numquam ipsum enim.
               </p>
-              <button onClick={() => console.log('connect')}>
-                Let's connect <ArrowRightCircle size={25}/>
-              </button>
+              <Link
+                to='#'
+                onClick={(e) => {
+                  window.location.href = mailto
+                  e.preventDefault()
+                }}
+              >
+                <button onClick={() => console.log('connect')}>
+                  Let's connect <ArrowRightCircle size={25} />
+                </button>
+              </Link>
             </Col>
             <Col xs={12} md={6} xl={5}>
-                <img src={headerImg} alt="header img" />
+              <img src={headerImg} alt='header img' />
             </Col>
           </Row>
         </Container>
