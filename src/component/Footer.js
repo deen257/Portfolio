ad03db94eb0e282d527e46adfc0433382b9a1106
ayomide <folorunshoayomide46@gmail.com>
2022-10-11 23:42:06 +0100

    updated footer section

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col } from 'react-bootstrap'
 import React from 'react'
 import logo from '../asset/img/logo.svg'
-import navIcon1 from '../asset/img/nav-icon1.svg'
-import navIcon2 from '../asset/img/nav-icon2.svg'
-import navIcon3 from '../asset/img/nav-icon3.svg'
+import linkedin from '../asset/img/nav-icon1.svg'
+import twitter from '../asset/img/twitter.svg'
+import git from '../asset/img/git.svg'
 
 
 
@@ -17,14 +17,14 @@ const Footer = () => {
           </Col>
           <Col size={12} sm={6} className='text-center text-sm-end'>
             <div className='social-icon'>
-              <a href='#'>
-                <img src={navIcon1} alt='Icon' />
+              <a href='https://www.linkedin.com/in/folorunsho-ayomide-2369441b5/'>
+                <img src={linkedin} alt='Icon' />
               </a>
-              <a href='#'>
-                <img src={navIcon2} alt='Icon' />
+              <a href='https://twitter.com/Ayomid_'>
+                <img src={twitter} alt='Icon' />
               </a>
-              <a href='#'>
-                <img src={navIcon3} alt='Icon' />
+              <a href='https://github.com/deen257'>
+                <img src={git} alt='Icon' />
               </a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
